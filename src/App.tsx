330252d6601caa7ffe78185ec2f5976832f47a17
Thanh1610 <Tatuanthanh2105nd@gmail.com>
2025-06-03 12:12:59 +0700

    React router v7

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { publicRoutes } from './routes';
+import MainLayout from './layouts/MainLayout/MainLayout';
+import { Fragment } from 'react/jsx-runtime';
 
-function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  )
+export function App() {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Layout = route.layout === null ? Fragment : route.layout || MainLayout;
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
-
-export default App
